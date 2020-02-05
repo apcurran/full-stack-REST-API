dcf507d24e2005bceffe6e8acb07b86cc2926547
apc <alexcurran.va@gmail.com>
2020-02-05 11:01:08 -0600

    Connected to DB

--- server.js
+++ server.js
@@ -3,20 +3,33 @@ const app = express();
 const logger = require("morgan");
 const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
+// Initialize dotenv
+require("dotenv").config();
 
 // Import routes
 
-require("dotenv").config();
+// DB Setup
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
 // Use Morgan for dev logging
 app.use(logger("dev"));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// Handle CORS
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-});
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+
+        return res.status(200).json({});
+    }
+
+    next();
+});
 // Handle 404 Error
 app.use((req, res, next) => {
     const error = new Error("Not found");
@@ -24,7 +37,6 @@ app.use((req, res, next) => {
 
     next(error);
 });
-
 // Catch other errors
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
