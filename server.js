9da88b20e623da819b5127e027a545657de1daf3
apc <alexcurran.va@gmail.com>
2020-02-05 10:43:52 -0600

    Added error handling

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require("express");
-const logger = require("morgan");
 const app = express();
+const logger = require("morgan");
 const PORT = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+
+// Import routes
 
 require("dotenv").config();
 
 // Use Morgan for dev logging
 app.use(logger("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+});
+
+// Handle 404 Error
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+
+    next(error);
+});
+
+// Catch other errors
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: err.message });
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
