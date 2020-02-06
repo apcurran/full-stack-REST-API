1fe87df8b11016713cc2e58609431fbaf2630d5b
apc <alexcurran.va@gmail.com>
2020-02-06 17:29:11 -0600

    Added House model

---
+++ api/models/House.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const HouseSchema = new mongoose.Schema({
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: String, required: true },
+    bedrooms: { type: Number, required: true },
+    bathrooms: { type: Number, required: true },
+    squareFeet: { type: Number, required: true },
+    description: { type: String, required: true },
+    agent: { type: String, required: true }
+});
+
+module.exports = mongoose.model("House", HouseSchema);
--- api/models/User.js
+++ api/models/User.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true, min: 1, max: 1000 },
+    email: { type: String, required: true, min: 1, max: 1000 },
+    password: { type: String, required: true, min: 6, max: 1000 },
     secret: { type: String },
-    admin: { type: Boolean, default: false },
-    password: { type: String, required: true, min: 6, max: 1000 }
+    admin: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model("User", UserSchema);
