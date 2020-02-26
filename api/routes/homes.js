bd609bca2bc8989c5c4135a29e2e6c875b3721af
apc <alexcurran.va@gmail.com>
2020-02-26 12:18:13 -0600

    Working on Multer image upload functionality, currently testing

--- api/routes/homes.js
+++ api/routes/homes.js
@@ -8,7 +8,15 @@ const { check } = require("express-validator");
 const checkAuth = require("../middleware/check-auth");
 const validate = require("../middleware/validate");
 const paginatedResults = require("../middleware/paginatedResults");
+
 const multer = require("multer");
+const upload = multer({ dest: 'uploads/' });
+const cpUpload = upload.fields([
+    { name: "agent_img", maxCount: 1 },
+    { name: "house_img_main", maxCount: 1 },
+    { name: "house_img_inside_1", maxCount: 1 },
+    { name: "house_img_inside_2", maxCount: 1 },
+]);
 
 // GET all homes
 router.get("/", paginatedResults(House), async (req, res, next) => {
@@ -37,51 +45,53 @@ router.get("/:homeId", async (req, res, next) => {
 router.post("/new", checkAuth, validate([
 
     // Validate req data
-    check("price").notEmpty().escape(),
-    check("street").notEmpty().escape(),
-    check("city").notEmpty().escape(),
-    check("state").notEmpty().escape(),
-    check("zip").notEmpty().escape(),
-    check("lat").notEmpty(),
-    check("lon").notEmpty(),
-    check("bedrooms").notEmpty().escape().isNumeric(),
-    check("bathrooms").notEmpty().escape().isNumeric(),
-    check("squareFeet").notEmpty().escape().isNumeric(),
-    check("description").notEmpty().escape(),
-    check("agent").notEmpty().escape(),
-    check("agent_img").notEmpty(),
-    check("agent_phone").notEmpty(),
-    check("house_img_main").notEmpty(),
-    check("house_img_inside_1").notEmpty(),
-    check("house_img_inside_2").notEmpty()
+    // check("price").notEmpty().escape(),
+    // check("street").notEmpty().escape(),
+    // check("city").notEmpty().escape(),
+    // check("state").notEmpty().escape(),
+    // check("zip").notEmpty().escape(),
+    // check("lat").notEmpty(),
+    // check("lon").notEmpty(),
+    // check("bedrooms").notEmpty().escape().isNumeric(),
+    // check("bathrooms").notEmpty().escape().isNumeric(),
+    // check("squareFeet").notEmpty().escape().isNumeric(),
+    // check("description").notEmpty().escape(),
+    // check("agent").notEmpty().escape(),
+    // check("agent_img").notEmpty(),
+    // check("agent_phone").notEmpty(),
+    // check("house_img_main").notEmpty(),
+    // check("house_img_inside_1").notEmpty(),
+    // check("house_img_inside_2").notEmpty()
 
-]), async (req, res, next) => {
+]), cpUpload, async (req, res, next) => {
     try {
-        const home = new House({
-            price: req.body.price,
-            street: req.body.street,
-            city: req.body.city,
-            state: req.body.state,
-            zip: req.body.zip,
-            lat: req.body.lat,
-            lon: req.body.lon,
-            bedrooms: req.body.bedrooms,
-            bathrooms: req.body.bathrooms,
-            squareFeet: req.body.squareFeet,
-            description: req.body.description,
-            agent: req.body.agent,
-            agent_img: req.body.agent_img,
-            agent_phone: req.body.agent_phone,
-            house_img_main: req.body.house_img_main,
-            house_img_inside_1: req.body.house_img_inside_1,
-            house_img_inside_2: req.body.house_img_inside_2,
-        });
+        console.log(req.files);
+        console.log(req.body);
+        // const home = new House({
+        //     price: req.body.price,
+        //     street: req.body.street,
+        //     city: req.body.city,
+        //     state: req.body.state,
+        //     zip: req.body.zip,
+        //     lat: req.body.lat,
+        //     lon: req.body.lon,
+        //     bedrooms: req.body.bedrooms,
+        //     bathrooms: req.body.bathrooms,
+        //     squareFeet: req.body.squareFeet,
+        //     description: req.body.description,
+        //     agent: req.body.agent,
+        //     agent_img: req.body.agent_img,
+        //     agent_phone: req.body.agent_phone,
+        //     house_img_main: req.body.house_img_main,
+        //     house_img_inside_1: req.body.house_img_inside_1,
+        //     house_img_inside_2: req.body.house_img_inside_2,
+        // });
 
-        await home.save();
+        // await home.save();
 
-        res.status(201).json({
-            message: "New home created!"
-        });
+        // res.status(201).json({
+        //     message: "New home created!"
+        // });
     } catch (err) {
         console.error(err);
         res.json({
