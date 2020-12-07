46d0ba29ac54d70b095e525c7a98e39cd810942a
Alex Curran <alexcurran.va@gmail.com>
2020-12-07 13:03:19 -0600

    Updated seeds.js to include randomuser.me api for agent names/thumbnails

--- populate-db/seeds.js
+++ populate-db/seeds.js
@@ -1,4 +1,4 @@
-// Specified database as argument - e.g.: seeds.js mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/my_cool_project?retryWrites=true')
+// Specified database as argument - e.g.: node seeds.js mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/my_cool_project?retryWrites=true'
 
 // Get arguments passed on command line
 var userArgs = process.argv.slice(2);
@@ -11,11 +11,12 @@ if (!userArgs[0].startsWith('mongodb')) {
 
 const House = require("../api/models/House");
 const faker = require("faker");
+const fetch = require("node-fetch");
 
 var mongoose = require('mongoose');
 var mongoDB = userArgs[0];
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -26,7 +27,17 @@ function randomIntFromRange(low, high) {
 async function populateDb() {
     try {
 
+        let homeArrPromises = [];
+
         for (let i = 0; i < 75; i++) {
+            // Agent info using Random User gen API
+            const agentInfoResponse = await fetch("https://randomuser.me/api/?format=json&inc=name,picture&nat=us,gb");
+            const agentInfoData = await agentInfoResponse.json();
+            const results = agentInfoData.results;
+            const agentFirstName = results[0].name.first;
+            const agentLastName = results[0].name.last;
+            const agentThumbnail = results[0].picture.large;
+
             const home = new House({
                 price: randomIntFromRange(150000, 600000),
                 street: faker.address.streetAddress(),
@@ -39,18 +50,20 @@ async function populateDb() {
                 bathrooms: randomIntFromRange(1, 3),
                 squareFeet: randomIntFromRange(1500, 4000),
                 description: faker.lorem.paragraph(),
-                agent: faker.name.findName(),
-                agent_img: faker.image.avatar(),
+                agent: `${agentFirstName} ${agentLastName}`,
+                agent_img: agentThumbnail,
                 agent_phone: faker.phone.phoneNumberFormat(),
                 house_img_main: `https://source.unsplash.com/${randomIntFromRange(700, 900)}x${randomIntFromRange(600, 700)}/?house`,
                 house_img_inside_1: "https://source.unsplash.com/800x600/?kitchen,living",
                 house_img_inside_2: "https://source.unsplash.com/799x600/?kitchen,living",
             });
     
-            await home.save();
+            homeArrPromises.push(home.save());
         }
     
+        await Promise.all(homeArrPromises);
         console.log("DB populated!");
+        return;
         
     } catch (err) {
         console.error(err);
