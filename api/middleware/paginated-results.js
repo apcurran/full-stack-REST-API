c0787f09c0120e5aab62a610a411709a71f783be
Alex Curran <alexcurran.va@gmail.com>
2021-02-09 09:42:03 -0600

    Tested Redis update working on paginatedResults middleware

--- api/middleware/paginated-results.js
+++ api/middleware/paginated-results.js
@@ -1,36 +1,55 @@
 "use strict";
 
+const redisClient = require("../../redis/index");
+
 module.exports = function(model) {
     return async function(req, res, next) {
         const page = Number(req.query.page);
         const limit = Number(req.query.limit);
 
-        const startIndex = (page - 1) * limit;
-        const endIndex = page * limit;
-
-        const results = {};
-
-        if (endIndex < await model.countDocuments().lean().exec()) {
-            results.next = {
-                page: page + 1,
-                limit: limit
-            };
-        }
-
-        if (startIndex > 0) {
-            results.previous = {
-                page: page - 1,
-                limit: limit
-            };
-        }
-
         try {
-            results.results = await model
+            const startIndex = (page - 1) * limit;
+            const endIndex = page * limit;
+            
+            const results = {};
+            
+            if (endIndex < await model.countDocuments().lean().exec()) {
+                results.next = {
+                    page: page + 1,
+                    limit: limit
+                };
+            }
+            
+            if (startIndex > 0) {
+                results.previous = {
+                    page: page - 1,
+                    limit: limit
+                };
+            }
+            
+            // Redis key
+            const cacheKey = `homes:results:page:${page}`;
+            let cacheEntry = await redisClient.get(cacheKey);
+
+            if (cacheEntry) {
+                cacheEntry = JSON.parse(cacheEntry);
+                results.results = cacheEntry;
+
+                res.paginatedResults = results;
+                return next();
+            }
+
+            const homesResults = await model
                 .find()
                 .limit(limit)
                 .skip(startIndex)
                 .lean()
                 .exec();
+
+            results.results = homesResults;
+
+            // Cache entry in Redis for future re-use
+            redisClient.set(cacheKey, JSON.stringify(homesResults), "EX", 3600); // TTL set to 1 hr
 
             res.paginatedResults = results;
             next();
